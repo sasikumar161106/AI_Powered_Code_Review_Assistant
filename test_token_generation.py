e8de1da95fcb2d8db07374fd2e017f99e56de119
sasikumar161106 <sasikumarldrago@gmail.com>
2026-06-07 09:35:14 +0530

    chore: remove test scripts and unwanted files

--- test_token_generation.py
+++
@@ -1,9 +0,0 @@
-from github import GithubIntegration
-app_id = '3970625'
-key = open('github-private-key.pem', 'r').read()
-integration = GithubIntegration(app_id, key)
-try:
-    token = integration.get_access_token(138387450).token
-    print("PyGithub Token Success")
-except Exception as e:
-    print("PyGithub Token Error:", e)
